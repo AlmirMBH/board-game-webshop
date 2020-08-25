35442315cb1b826c12cc3d138fd1193af2426881
AlmirMBH <almir.mustafic.tz@gmail.com>
2020-08-25 15:33:20 +0200

    Created database tables and performed their migrations

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'MasterController@home')->name('home');
+Route::get('/home', 'PagesController@home')->name('home');
 
-Route::get('/uber-uns', 'MasterController@uber_uns')->name('about-us');
+Route::get('/uber-uns', 'PagesController@uber_uns')->name('about-us');
 
-Route::get('/web-shop', 'MasterController@web_shop')->name('webshop');
+Route::get('/web-shop', 'PagesController@web_shop')->name('webshop');
 
-Route::get('/lizenznehmer', 'MasterController@lizenznehmer')->name('license-providers');
+Route::get('/lizenznehmer', 'PagesController@lizenznehmer')->name('license-providers');
 
-Route::get('/teilnehmende-betriebe', 'MasterController@betriebe')->name('participating-companies');
+Route::get('/teilnehmende-betriebe', 'PagesController@betriebe')->name('participating-companies');
 
-Route::get('/kontakt', 'MasterController@kontakt')->name('contact');
+Route::get('/kontakt', 'PagesController@kontakt')->name('contact');
