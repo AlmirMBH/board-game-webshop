2d5170afcfe5e2a956968a7bbf2a4d6b0be12b00
SamerSacic <samer.sacic@gmail.com>
2020-08-31 18:22:08 +0200

    Add address information in kontak page, replace google map

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -67,9 +67,20 @@
                 </div>
             </div>
             <div class="col-sm-12 col-md-12 col-lg-5">
+                <div class="address">
+                    <h3>GEWERBE-SPIEL.ch</h3>
+                    <h4>c/o Rüegg Management GmbH.</h4>
+                    <p><i class="fas fa-map-marker-alt"></i> Waldegg 14a, 8623 Wetzikon</p>
+                    <p><i class="fas fa-phone"></i> Tel. +41 78 680 90 40</p>
+                    <p><i class="fas fa-envelope"></i> <a href="mailto:info@gewerbe-spiel.ch">info@gewerbe-spiel.ch</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-12">
                 <div class="contact-map">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d86456.4062071105!2d8.466675517418452!3d47.377549914955246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900b9749bea219%3A0xe66e8df1e71fdc03!2sZ%C3%BCrich%2C%20Switzerland!5e0!3m2!1sen!2sba!4v1598030207823!5m2!1sen!2sba"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2704.262435188095!2d8.825348615467695!3d47.328745815124144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ab95c206c6395%3A0xf61bd7488e47480b!2sWaldegg%2014A%2C%208623%20Wetzikon%2C%20Switzerland!5e0!3m2!1sen!2sba!4v1598890202286!5m2!1sen!2sba"
                         width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""
                         aria-hidden="false"
                         tabindex="0"></iframe>
