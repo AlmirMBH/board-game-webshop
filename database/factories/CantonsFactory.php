bf03f36ebf8f496b1c8541c1f7de21c95b6205b6
Strayko <strayko_lol@windowslive.com>
2020-08-31 15:58:20 +0200

    fix

--- database/factories/CantonsFactory.php
+++ database/factories/CantonsFactory.php
@@ -4,9 +4,42 @@
 
 use App\Canton;
 use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
 
 $factory->define(Canton::class, function (Faker $faker) {
-    return [
-      'name' => $faker->randomElement($array = array('ZH', 'SH', 'TG', 'SG', 'AR', 'AI', 'LI', 'ZG', 'SZ', 'GL', 'LU', 'NW', 'OW', 'UR', 'GR', 'BE', 'BS', 'BL', 'AG', 'SO'))
-    ];
+  $canton = new Canton();
+  $canton->name = 'ZH';
+  $canton->save();
+
+  $canton2 = new Canton();
+  $canton2->name = 'SH+TG';
+  $canton2->save();
+
+  $canton3 = new Canton();
+  $canton3->name = 'SG+AR+AI+LI';
+  $canton3->save();
+
+  $canton4 = new Canton();
+  $canton4->name = 'ZG+SZ+GL';
+  $canton4->save();
+
+  $canton5 = new Canton();
+  $canton5->name = 'LU+NW+OW+UR';
+  $canton5->save();
+
+  $canton6 = new Canton();
+  $canton6->name = 'GR';
+  $canton6->save();
+
+  $canton7 = new Canton();
+  $canton7->name = 'BE';
+  $canton7->save();
+
+  $canton8 = new Canton();
+  $canton8->name = 'BS+BL';
+  $canton8->save();
+
+  $canton9 = new Canton();
+  $canton9->name = 'AG+SO';
+  $canton9->save();
 });
