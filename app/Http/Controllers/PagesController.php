dd6cdfe8d6f75a00b458a1f888f42c2989794575
SamerSacic <samer.sacic@gmail.com>
2020-09-02 18:25:50 +0200

    Page controller refactor methods

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,54 +10,59 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
 
-  public function home(){
-    return view('index');
-  }
+    public function home()
+    {
+        return view('index');
+    }
+    
+    public function uber_uns()
+    {
+        return view('uber-uns');
+    }
 
+    public function web_shop()
+    {
+        return view('web-shop');
+    }
 
-   public function uber_uns(){
-     return view('uber-uns');
-   }
+    public function lizenznehmer()
+    {
+        return view('lizenznehmer');
+    }
 
-  public function web_shop(){
-    return view('web-shop');
-  }
+    public function betriebe()
+    {
+        return view('teilnehmende-betriebe');
+    }
 
-  public function lizenznehmer(){
-    return view('lizenznehmer');
-  }
+    public function kontakt()
+    {
+        return view('kontakt');
+    }
 
-  public function betriebe(){
-    return view('teilnehmende-betriebe');
-  }
+    public function list_lizenznehmer()
+    {
+        $cantons = Canton::all();
+        return view('list-lizenznehmer', compact('cantons'));
+    }
 
-  public function kontakt(){
-     return view('kontakt');
-  }
+    public function get_provider($slug)
+    {
+        $provider = Provider::where('slug', $slug)->firstOrFail();
+        return view('provider', compact('provider'));
+    }
 
-  public function list_lizenznehmer()
-  {
-    $cantons = Canton::all();
-    return view('list-lizenznehmer', compact('cantons'));
-  }
+    public function get_place($slug)
+    {
+        $place = Outlets::where('slug', $slug)->firstOrFail();
+        return view('point-of-sale-details', compact('place'));
+    }
 
-  public function get_provider($slug)
-  {
-    $provider = Provider::where('slug', $slug)->firstOrFail();
-    return view('provider', compact('provider'));
-  }
-
-  public function get_place($slug)
-  {
-    $place = Outlets::where('slug', $slug)->firstOrFail();
-    return view('point-of-sale-details', compact('place'));
-  }
-
-  public function outlets()
-  {
-    $cantons = Canton::all();
-    return view('outlets', compact('cantons'));
-  }
+    public function outlets()
+    {
+        $cantons = Canton::all();
+        return view('outlets', compact('cantons'));
+    }
 
 }
 
