02c3d172b9686e28f27e4487d7b2d4b0cbc20553
SamerSacic <samer.sacic@gmail.com>
2020-09-04 00:08:30 +0200

    Inital commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -168,31 +168,31 @@
 </header>
 <!-- END Main header -->
 
-<main id="app">
-    @yield('content')
-</main>
+    `<main id="app">
+        @yield('content')
+    </main>
 
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 text-center py-4">
-                <p class="mb-0 text-white">Copyright 2020, Gewerbe Spiel, Alle Rechte vorbehalten</p>
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 text-center py-4">
+                    <p class="mb-0 text-white">Copyright 2020, Gewerbe Spiel, Alle Rechte vorbehalten</p>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
-<script
-    src="https://code.jquery.com/jquery-3.5.1.min.js"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+            crossorigin="anonymous"></script>
 
-@yield('script')
+    @yield('script')`
 </body>
 </html>
